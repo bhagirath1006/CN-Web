f811a2242ed979ec9240e7b46fa1169fb137e7ab
bhagirath1006 <bhagi.sy99@gmal.com>
2026-01-05 03:58:01 +0530

    Simplify  setup

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -28,7 +28,7 @@ variable "enable_vault" {
 variable "key_name" {
   description = "SSH key pair name for EC2 instance access"
   type        = string
-  default     = "my-key"
+  default     = "ram"
 }
 
 variable "vault_token" {
